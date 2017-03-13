97e3a0ad49cd2b00bd4f53850742961e82bce5d0
Aaron Gaba <airplane679@gmail.com>
2017-03-13 12:15:37 -0400

    Refactored JS, Added Readme

--- windowsizer.js
+++ windowsizer.js
@@ -4,13 +4,17 @@
 
 /* Note: \u00D7 is the Unicode representation for the 'times' (x) symbol. */
 
+function prettyPrintDimensions(height, width) {
+	return (width + " \u00D7 " + height);
+}
+
 window.onload = function(){
-    document.getElementById("screen-size").innerHTML = screen.width + " \u00D7 " + screen.height;
-    document.getElementById("window-size").innerHTML = window.innerWidth + " \u00D7 " + window.innerHeight;
+	document.getElementById("screen-size").innerHTML = prettyPrintDimensions(screen.height, screen.width);
+    document.getElementById("window-size").innerHTML = prettyPrintDimensions(window.innerHeight, window.innerWidth);
 }
 
 window.onresize = function(){
-    document.getElementById("window-size").innerHTML = window.innerWidth + " \u00D7 " + window.innerHeight;
+    document.getElementById("window-size").innerHTML = prettyPrintDimensions(window.innerHeight, window.innerWidth);
 }
 
 
